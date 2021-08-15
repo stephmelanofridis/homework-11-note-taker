2411c37941b586233147d702417220246b029638
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-08-15 15:55:17 +1000

    Added functionality to delete notes

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const uniqid = ("uniqid");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (request, response) => response.send("Click here to start"));
+
 app.get("/notes", (request, response) => {
-    fs.sendFile(path.join(__dirname, "public/notes.html"));
+    response.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("/api/notes", (request, response) => {
@@ -25,10 +27,9 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.post("/api/notes", (request, response) => {
-    console.log("A posted note. ");
-    console.log(request.body);
-    response.json(request.body);
-    const newNote = request.body;
+    const { title, text } = request.body;
+    const newNote = { title, text, id: uniqueid() };
+    response.json(newNote);
 
     fs.readFile("./db/db.json", "UTF-8", (err, data) => {
         if (err) {
@@ -42,5 +43,21 @@ app.post("/api/notes", (request, response) => {
     });
 });
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}.`));
+app.delete("api/notes/:id", (request, response) => {
+    const deteleId = require.params.id
+    const isNotId = (i) => i.id != deleteId
+
+    fs.readFile("./db/db.json", "UTF-8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+            response.json(isDeleted);
+
+            fs.writeFile("./db/db.json", JSON.stringify(isDeleted), (writeErr) => writeErr ? console.error(writeErr) : console.info("Note deleted. "));
+        }
+    });
+});
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}.`));
 
